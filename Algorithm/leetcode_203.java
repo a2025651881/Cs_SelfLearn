2aa139743512c7dc213f713adbba429fd1db99b5
a2025651881 <2025651881@qq.com>
2023-11-19 21:37:54 +0800

    leetcode_203

---
+++ Algorithm/leetcode_203.java
@@ -0,0 +1,25 @@
+public class leetcode_203 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // sentinel节点保持一致性
+        ListNode sentienalNode = new ListNode(0, head);
+        ListNode point = sentienalNode;
+        while (point.next != null) {
+            if (point.next.val == val) {
+                point.next = point.next.next;
+            } else {
+                point = point.next;
+            }
+        }
+        return sentienalNode.next;
+    }
+}
